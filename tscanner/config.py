dd4b01acce2393bd5e7b308d83937093219fb362
chushuai <1040641785@qq.com>
2019-04-18 11:10:50 +0800

    modify technical architecture

---
+++ tscanner/config.py
@@ -0,0 +1,7 @@
+
+mongo_settings = {
+  "ip":'127.0.0.1',   #ip
+  "port":27017,
+  "db_name" : "mydb",
+  "set_name" : "test_set"
+}
--- tscanner/scheduler.py
+++ tscanner/scheduler.py
@@ -1,6 +1,37 @@
+#coding:utf-8
+import time
+
+
 class Scheduler:
+    
     def __init__(self):
-       print ("--")
+        print "--"
+        self.task_list=[]
+    
+       
+    def working(self):
+        print "--->"
+        #time1 query  updatetime > time1 
+        #last_time<updatetime<next_time
+        #last_time=time.now
+        while(True):
+           #time1+2 >
+           #step 1: 
+           #last_time=next_time
+           #next_time=time.now
+           ##last_time<=updatetime<next_time
+           print "Get new tasks"
+           
+           #step 2:
+           print "check task status"
+           
+           time.sleep(1)
+        
+        
+if __name__ == "__main__":
+    print "++hellow"
+    sche = Scheduler()
+    sche.working()
     
     
     
