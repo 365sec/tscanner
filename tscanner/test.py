c7446dc9b205c938741e064da891578367084370
llhfromgrxa <1285083123@qq.com>
2019-04-22 14:35:25 +0800

    add schu func

--- tscanner/test.py
+++ tscanner/test.py
@@ -4,9 +4,9 @@ import json
 import time
 import requests
 
-#res1 = requests.post("http://127.0.0.1:5000/task/new", data={"task_name":"task_name","scan_cfg":{}})
-#res1 = requests.post("http://127.0.0.1:5000/task/delete", data={"taskid":"2584de8f-5aa1-11e9-b2f9-b88687bf8290"})
+#res1 = requests.post("http://127.0.0.1:5000/task/new", data={"task_name":"task1","scan_cfg":{}})
+#res1 = requests.post("http://127.0.0.1:5000/task/delete", data={"taskid":"baf102cf-61b6-11e9-8ef6-e0d55e8f99a0"})
 #res1 = requests.post("http://127.0.0.1:5000/task/report", data={"taskid":"8e0e77c0-5aa4-11e9-b0d2-b88687bf8290"})
-res1 = requests.post("http://127.0.0.1:5000/task/stop", data={"taskid":"dd32f09e-5abe-11e9-b17a-b88687bf8290"})
-#res1 = requests.post("http://127.0.0.1:5000/task/status", data={"taskid":"8e0e77c0-5aa4-11e9-b0d2-b88687bf8290"})
+#res1 = requests.post("http://127.0.0.1:5000/task/stop", data={"taskid":"b2a321b0--11e-e0d55e8f99a0"})
+res1 = requests.post("http://127.0.0.1:5000/task/status", data={"taskid":"6827ef80-61ea-11e9-96ce-b88687bf8290"})
 print ("ask_new:",res1.json())
